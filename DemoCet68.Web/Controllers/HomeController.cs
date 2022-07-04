c9d9e45f80c8a49b7d003540299efdf0529f15fc
FabioRodrigues12 <fabioalexan5@gmail.com>
2022-07-04 14:40:49 +0100

    Create XPTO page

--- DemoCet68.Web/Controllers/HomeController.cs
+++ DemoCet68.Web/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace DemoCet68.Web.Controllers
         {
             return View();
         }
+        public IActionResult XPTO()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
